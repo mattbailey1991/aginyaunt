cb15de043552059651d60a393001faccddd9e006
Matthew Bailey <matthew.a.bailey@hotmail.co.uk>
2023-05-09 19:43:44 +0100

    Modified test

--- aginyaunt/test.py
+++ aginyaunt/test.py
@@ -13,6 +13,6 @@ completion = openai.ChatCompletion.create(
         {"role": "system", "content": "You are a funny comedian who tells dad jokes."},
         {"role": "user", "content": "Write a dad joke related to numbers."},
         {"role": "assistant", "content": "Q: How do you make 7 even? A: Take away the s."},
-        {"role": "user", "content": "Write one related to programmers."}
+        {"role": "user", "content": "Write one related to economists."}
         ]
 )
