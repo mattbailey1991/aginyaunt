075b34558410954d1c6584aee96e298eb52acfa6
Matthew Bailey <matthew.a.bailey@hotmail.co.uk>
2023-08-15 20:14:44 +0100

    FEAT: Root redirects to /aginy

--- aginyaunt/urls.py
+++ aginyaunt/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('aginy/', include('aginy.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('', RedirectView.as_view(url="aginy/"))
 ]
